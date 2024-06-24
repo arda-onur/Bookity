89c3780eb785051f56fc37b96488cd024140aaba
Berk Ekim Pasmakoglu <berkekim@gmail.com>
2024-06-24 22:33:10 +0300

    Make flyway integration

--- src/main/java/com/bookity/project/candidate/arda/onur/persistence/model/Book.java
+++ src/main/java/com/bookity/project/candidate/arda/onur/persistence/model/Book.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Book {
     @Id
+    private Long id;
     private String isbn;
     private String bookName;
     private String category;
---
+++ src/main/resources/db/migration/V1_0_0__create_book.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS bookity.book
+(
+    "id"        SERIAL PRIMARY KEY,
+    "isbn"      VARCHAR(255) NOT NULL,
+    "book_name" VARCHAR(255) NOT NULL,
+    "category"  VARCHAR(255) DEFAULT NULL,
+    "image_url" VARCHAR(255) DEFAULT NULL,
+);
