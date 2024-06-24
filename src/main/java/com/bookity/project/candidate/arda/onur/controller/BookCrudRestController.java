1dfb2c93395d87356791dcc6a59b868fdef3bc14
Berk Ekim Pasmakoglu <berkekim@gmail.com>
2024-06-24 21:45:31 +0300

    Add one-argument constructor for injection

--- src/main/java/com/bookity/project/candidate/arda/onur/controller/BookCrudRestController.java
+++ src/main/java/com/bookity/project/candidate/arda/onur/controller/BookCrudRestController.java
@@ -2,7 +2,7 @@ package com.bookity.project.candidate.arda.onur.controller;
 
 import Project.bookity.Entity.BookEntity;
 import Project.bookity.Service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,9 +12,9 @@ import java.util.List;
 
 @RestController
 @RequestMapping("books")
+@RequiredArgsConstructor
 public class BookCrudRestController {
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
 
     @GetMapping
     public List<BookEntity> getAllBooks() {
