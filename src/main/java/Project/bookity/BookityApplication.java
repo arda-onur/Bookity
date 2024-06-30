332c4a42c00050c1153d1403c00ba18cc2a8f65e
Berk Ekim Pasmakoglu <berkekim@gmail.com>
2024-06-30 17:10:58 +0300

    Fix unnecessary empty lines

--- src/main/java/Project/bookity/BookityApplication.java
+++ src/main/java/Project/bookity/BookityApplication.java
@@ -9,9 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"com.bookity.project.candidate.arda.onur.persistence.repository"})
 @EntityScan("com.bookity.project.candidate.arda.onur")
 public class BookityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BookityApplication.class, args);
 	}
-
 }
