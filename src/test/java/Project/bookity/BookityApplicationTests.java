2e15fe733a8ce1582cece611d0bce7166048314f
Berk Ekim Pasmakoglu <berkekim@gmail.com>
2024-06-30 17:04:48 +0300

    Correct maven  wrapper setup

--- src/test/java/Project/bookity/BookityApplicationTests.java
+++ src/test/java/Project/bookity/BookityApplicationTests.java
@@ -1,8 +1,6 @@
 package Project.bookity;
 
 import com.bookity.project.candidate.arda.onur.controller.BookCrudRestController;
-import com.bookity.project.candidate.arda.onur.persistence.repository.BookRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +13,8 @@ class BookityApplicationTests {
     BookCrudRestController bookCrudRestController;
 
     @Test
-    void contextLoads() {
-        assertThat(this.bookCrudRestController).isNotNull();
+    void shouldLoadApplicationContextWithBookCrudRestController() {
+        assertThat(this.bookCrudRestController)
+            .isNotNull();
     }
 }
