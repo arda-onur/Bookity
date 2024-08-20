c992030432f328b797c52abdc1446fa3937610b3
arda-onur <ardaonur15@gmail.com>
2024-08-20 10:13:31 +0300

    Added tests for User

--- src/main/java/com/bookity/project/candidate/arda/onur/BookityApplication.java
+++ src/main/java/com/bookity/project/candidate/arda/onur/BookityApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookityApplication {
+public class  BookityApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookityApplication.class, args);
     }
