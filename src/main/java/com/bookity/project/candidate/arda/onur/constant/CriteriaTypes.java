1581e16a5b5be9f8731bac49c9d69d49f9c7079f
Arda Onur <104937816+arda-onur@users.noreply.github.com>
2024-12-03 12:02:17 +0300

    Update CriteriaTypes.java

--- src/main/java/com/bookity/project/candidate/arda/onur/constant/CriteriaTypes.java
+++ src/main/java/com/bookity/project/candidate/arda/onur/constant/CriteriaTypes.java
@@ -1,10 +1,4 @@
-/**
- * [REGISTER]
- * Project : bookity
- * User    : pasmakoglu
- * Date    : 3/9/2019
- * Time    : 6:23 AM
- */
+
 package com.bookity.project.candidate.arda.onur.constant;
 
 import lombok.Getter;
